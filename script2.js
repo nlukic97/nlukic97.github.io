6f04443d29342b3e17b953bf940c41ac200a76dc
Nikola Lukic <nlukic97@gmail.com>
2021-09-05 20:54:29 +0200

    pretty functional from what I can see, still need to work on the echo function maybe, maybe not

--- script2.js
+++ script2.js
@@ -8,7 +8,7 @@ var cmdHistory = []
 var cmdIndex = 0;
 
 function addFolder(location,folderName){
-    if(folderExists(location[folderName])){
+    if(folder_file_exists(location[folderName])){
         outputText('This folder already exists.')
     } else {
         location[folderName] = new Object();
@@ -16,18 +16,32 @@ function addFolder(location,folderName){
 }
 
 function addFile(location,filename){
-    location[filename] = ''
+    if(folder_file_exists(location[filename])){
+        outputText('This name is already taken by a file or a folder.')
+    } else {
+        location[filename] = ''
+    }
 }
 
 function removeFolder(location,folderName){
-    if(folderExists(location[folderName])){
+    console.log(location[folderName]);
+    if(folder_file_exists(location[folderName]) && typeof(location[folderName]) === 'object'){
         delete location[folderName]
+    } else {
+        outputText('Unable to delete non-existing folder.')
+    }
+}
+
+function removeFile(location,fileName){
+    if(typeof(location[fileName]) === 'string'){
+        
+        delete location[fileName]
     } else {
         outputText('Unable to delete non-existing file.')
     }
 }
 
-function folderExists(folder){
+function folder_file_exists(folder){
     if(folder){
         return true
     } else {
@@ -37,8 +51,6 @@ function folderExists(folder){
 
 function showLastCommand(){
     if(cmdHistory.length > 0){
-        console.log(cmdIndex);
-        console.log(cmdHistory[cmdIndex]);
         document.getElementById('terminal-textbox').value = cmdHistory[cmdIndex]
         
         cmdIndex++;
@@ -54,6 +66,8 @@ addFolder(pc['/'],'root')
 
 addFolder(pc['/']['users'],'Nikola')
 addFolder(pc['/']['users'],'Ubuntu')
+
+addFile(pc['/'],'txt')
 
 document.addEventListener('click',e=>{
     document.getElementById('terminal-textbox').focus()
@@ -72,10 +86,14 @@ document.getElementById('terminal-textbox').addEventListener('keypress',(e)=>{
     }
 })
 
-document.addEventListener('keydown',(e)=>{
+document.getElementById('terminal-textbox').addEventListener('keydown',(e)=>{
     if(e.key==='ArrowUp'){
         showLastCommand()
-        console.log('arrow up');
+    }
+    
+    if(e.key == 'Tab' && document.getElementById('terminal-textbox').value.includes('cd ')){
+        e.preventDefault()
+        autofill(document.getElementById('terminal-textbox').value.substring(3))
     }
 })
 
@@ -92,11 +110,20 @@ function scrollToBottom(){
 function ls(){
     var string = '';
     Object.keys(pwd).forEach(key=>{
-        console.log(key);
         string = string + key + ' '
     });
     
-    console.log(string);
+    outputText(string)
+}
+
+function showFolderList(){
+    var string = '';
+    Object.keys(pwd).forEach(key=>{
+        if(typeof(pwd[key])==='object'){
+            string = string + key + ' '
+        }
+    });
+    
     outputText(string)
 }
 
@@ -107,6 +134,17 @@ function cd(arg){ //bug when I am in the Nikola folder.
     
     var keys = Object.keys(pwd);
     let ans = keys.includes(arg)
+    
+    if(ans === false){
+        outputText('Error: The directory could not be found.')
+        return
+    }
+    
+    if(typeof(pwd[arg]) === 'string'){
+        outputText('Error: The directory name is invalid.')
+        return
+    }
+    
     if(ans === true){
         pwd = pwd[arg]
         pwdPath.push(arg)
@@ -118,10 +156,8 @@ function execute(){
     let cmd = document.getElementById('terminal-textbox').value
     cmdHistory.unshift(cmd)
     
-    console.log(cmdHistory);
-    
     outputText(getPwdPath(),true)
-    outputText(cmd)
+    outputText(cmd,false,true)
     
     if(cmd === 'ls'){
         ls()
@@ -137,10 +173,10 @@ function execute(){
         outputText('There is no real reason why I made this, seemed kinda cool.')
     } else if(cmd.includes('mkdir ')){
         addFolder(pwd,`${cmd.substring(6)}`)
-    } else if(cmd.includes('rmdir ') ){ //WORKS THE SAME AS RM, SAME AS RMDIR
-        removeFolder(pwd,cmd.substring(6)) //WORKS THE SAME AS RM, SAME AS RMDIR
+    } else if(cmd.includes('rmdir ') ){
+        removeFolder(pwd,cmd.substring(6)) //Does not work for some reason
     } else if(cmd.includes('rm ') ){
-        removeFolder(pwd,cmd.substring(3))
+        removeFile(pwd,cmd.substring(3))
     } else if(cmd.includes('echo ') && cmd.includes(' > ')){
         saveTextToFile(cmd)
         
@@ -186,15 +222,22 @@ function getPwdPath(){
     return 'C:'+pwdPath.join('/').replace('/','')
 }
 
-function outputText(text,color){
+function outputText(text,color,cmd){
     let a = document.createElement('div')
-    a.innerText = text
+    
+    if(cmd === true){
+        a.innerHTML = '&lambda; '+text
+    } else {
+        a.innerHTML = text
+    }
+    
     if(color === true){
         a.classList.add('location-color')
     } else {
         a.classList.add('text-color')
     }
     a.classList.add('output')
+    
     document.getElementById('output-container').append(a)
 }
 
@@ -204,23 +247,49 @@ function saveTextToFile(cmd){
         return outputText('Error')
     }
     var string = ''
-
+    
     let file = a[a.length - 1]
     a.forEach((item,index)=>{
         if(index != 0 && index < a.length - 2){
             string += ' ' + item
         }
     })
-
-    console.log(file);
+    
     pwd[file] = string
 }
 
 
 function cat(file){
     if(typeof(pwd[file]) == 'string'){
-        outputText(pwd[file])
+        outputText(pwd[file],false,true)
     } else {
         outputText('Unable to read.')
     }
+}
+
+function autofill(string){
+    let newString = string.split(' ')
+    
+    if(newString.length === 1){
+        if(string !== '' && string[0] !== ' ' && string[string.length-1] !== ''){
+            let keys = Object.keys(pwd)
+            
+            for(let i = 0; i < keys.length; i++){
+                let ans = keys[i].match('^' + string)
+                console.log(ans);
+                if(ans != null){
+                    if(typeof(pwd[keys[i]]) === 'string'){
+                        return
+                    }
+                    document.getElementById('terminal-textbox').value = 'cd ' + keys[i]
+                    break 
+                }
+            }
+        } else {
+            outputText(getPwdPath(),true)
+            outputText('cd',false,true)
+            showFolderList()
+        }
+    }
+    
 }
